28cc5498f19e88e8949d0d7bb8a35f72bf5ad6f6
Omar Ernesto Cabrera Rosero <omar@poldrosky-laptop>
2014-04-24 21:35:09 -0500

    Test Tapas Cologne

--- Src/testTapas.sh
+++ Src/testTapas.sh
@@ -1,12 +1,9 @@
 python3 lcmFlock.py 16 10 5 Tapas.csv test1
 python3 lcmFlock.py 16 10 5 Tapas.csv test2
-python3 lcmFlock.py 16 10 5 Tapas.csv test3
 python3 lcmFlock.py 18 10 5 Tapas.csv test1
 python3 lcmFlock.py 18 10 5 Tapas.csv test2
-python3 lcmFlock.py 18 10 5 Tapas.csv test3
 python3 lcmFlock.py 20 10 5 Tapas.csv test1
 python3 lcmFlock.py 20 10 5 Tapas.csv test2
-python3 lcmFlock.py 20 10 5 Tapas.csv test3
 python3 lcmFlock.py 22 10 5 Tapas.csv test1
 python3 lcmFlock.py 22 10 5 Tapas.csv test2
 python3 lcmFlock.py 24 10 5 Tapas.csv test1
@@ -14,10 +11,8 @@ python3 lcmFlock.py 26 10 5 Tapas.csv test1
 python3 lcmFlock.py 28 10 5 Tapas.csv test1
 python3 bfe.py 16 10 5 Tapas.csv test1
 python3 bfe.py 16 10 5 Tapas.csv test2
-python3 bfe.py 16 10 5 Tapas.csv test3
 python3 bfe.py 18 10 5 Tapas.csv test1
 python3 bfe.py 18 10 5 Tapas.csv test2
-python3 bfe.py 18 10 5 Tapas.csv test3
 python3 bfe.py 20 10 5 Tapas.csv test1
 python3 bfe.py 20 10 5 Tapas.csv test2
 python3 bfe.py 22 10 5 Tapas.csv test1
