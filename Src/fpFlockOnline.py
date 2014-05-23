098b4d9b599d1b5e80cf8021a6b7703c45bfd28b
Omar Ernesto Cabrera Rosero <omar@od.(none)>
2014-05-23 17:28:32 -0500

    fpFlockOnline alpha

--- Src/fpFlockOnline.py
+++ Src/fpFlockOnline.py
@@ -29,8 +29,9 @@ import os
 import Pdbc
 import io
 import sys
+import copy
 
-class FPFlock(object):
+class FPFlockOnline(object):
     """ This class is intanced with epsilon, mu and delta"""
     def __init__(self, epsilon, mu, delta):
         self.epsilon = epsilon
@@ -38,6 +39,7 @@ class FPFlock(object):
         self.delta = delta
 
     def getTransactions(points, maximalDisks):
+        traj = {}
         for maximal in maximalDisks:
             for member in maximalDisks[maximal].members:
                 if not member  in traj.keys():
@@ -47,44 +49,34 @@ class FPFlock(object):
                     traj[member].append(maximalDisks[maximal].id)
         return traj
 	
-    def flocks(output1, totalMaximalDisks, keyFlock):
+    def flocks(traj):
+        if os.path.exists('output.dat'):
+                os.system('rm output.dat')
+        output = open('output.dat','w')
+        
+        for i in traj:
+                if len(traj[i]) == 1:
+                    continue
+                output.write(str(traj[i])+'\n')
+
+        output.close()
+
+        os.system("./fim_maximal output.dat " + str(LCMmaximal.mu) + " output.mfi > /dev/null")
+        
+        if os.path.exists('output.mfi'):
+            output1 = open('output.mfi','r')
+            				
         lines = output1.readlines()
         for line in lines:
             lineSplit = line.split(' ')
+            if len(lineSplit) <= 2:
+                continue               
             array = list(map(int,lineSplit[:-1]))
             array.sort()
-            if len(array) < delta:
-                continue
-            members = totalMaximalDisks[int(str(array[0]))].members
-            begin = totalMaximalDisks[int(str(array[0]))].timestamp
-            end = begin
-            for element in range(1,len(array)):
-                now = totalMaximalDisks[int(str(array[element]))].timestamp
-                if(now == end + 1 or now == end):
-                    if(now == end + 1):
-                        members = members.intersection(totalMaximalDisks[int(str(array[element]))].members)
-                    end = now
-					
-                elif end-begin >= delta - 1:
-                    b = list(members)
-                    b.sort()
-                    stdin.append('{0}\t{1}\t{2}\t{3}'.format(keyFlock, begin, end, b))
-                    keyFlock += 1
-                    begin = end = now
-
-                else:
-                    begin = end = now
-
-            if end-begin >= delta - 1:
-                b = list(members)
-                b.sort()
-                stdin.append('{0}\t{1}\t{2}\t{3}'.format(keyFlock, begin, end, b))
-                keyFlock += 1
-	
-        return stdin
+            print(array)
+            
 			
     def flockFinder(self,filename,tag):
-        global traj
         global stdin
         global delta
 
@@ -95,13 +87,9 @@ class FPFlock(object):
 
         dataset = csv.reader(open('Datasets/'+filename, 'r'),delimiter='\t')
 
-        if os.path.exists('output.dat'):
-            os.system('rm output.dat')		
-		
         if os.path.exists('output.mfi'):
             os.system('rm output.mfi')
 
-        output = open('output.dat','w')
         next(dataset)
 
         t1 = time.time()
@@ -114,56 +102,37 @@ class FPFlock(object):
         keyFlock = 1
         diskID = 1
 
-        traj = {}
-        totalMaximalDisks = {}
         stdin = []
+        
+        previousMaximalDisks = {}
 
         for timestamp in timestamps:
+            totalMaximalDisks = {}
             LCMmaximal.disksTimestamp(points, timestamp)
             if os.path.getsize('outputDisk.dat') == 0:
                 continue
-            maximalDisks, diskID = LCMmaximal.maximalDisksTimestamp(timestamp, diskID)
-            totalMaximalDisks.update(maximalDisks)
-
-            FPFlock.getTransactions(points, maximalDisks)
-
-        for i in traj:
-            if len(traj[i]) == 1:
-                continue
-            output.write(str(traj[i])+'\n')
-		
-        output.close()
-		
-        os.system("./fim_closed output.dat " + str(LCMmaximal.mu) + " output.mfi > /dev/null")
-		
-        if os.path.exists('output.mfi'):
-            output1 = open('output.mfi','r')				
-		
-            keyFlock = 1
-            stdin = FPFlock.flocks(output1, totalMaximalDisks, keyFlock)
-				
-        table = ('flocksFP')
-        print("Flocks: ",len(stdin))
-        flocks = len(stdin)
-        stdin = '\n'.join(stdin)
-        db = Pdbc.DBConnector()
-        db.createTableFlock(table)
-        db.resetTable(table.format(filename))
-        db.copyToTable(table,io.StringIO(stdin))
-		
+            currentMaximalDisks, diskID = LCMmaximal.maximalDisksTimestamp(timestamp, diskID)
+            totalMaximalDisks.update(currentMaximalDisks)
+            totalMaximalDisks.update(previousMaximalDisks)
+                                    
+            traj = FPFlockOnline.getTransactions(points, totalMaximalDisks)
+            
+            FPFlockOnline.flocks(traj)
+            
+            previousMaximalDisks = currentMaximalDisks
+            input("hola")
+            		
         t2 = round(time.time()-t1,3)
         print("\nTime: ",t2)
 		
-        db.createTableTest()
-        db.insertTest(filename,self.epsilon,self.mu, delta, t2, flocks, tag)
-		
+        		
 def main():
-    #fp = FPFlock(200,3,3)
+    fp = FPFlockOnline(200,3,3)
     #flockFinder('SJ2500T100t500f.csv')
-    #fp.flockFinder('Oldenburg.csv','fptest1')
+    fp.flockFinder('prueba.csv','fptest1')
 
-    fp = FPFlock(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
-    fp.flockFinder(str(sys.argv[4]),'fp1'+str(sys.argv[5]))
+    #fp = FPFlockOnline(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+    #fp.flockFinder(str(sys.argv[4]),'fp1'+str(sys.argv[5]))
 	
 if __name__ == '__main__':
     main()
