952043bb5c95ce7e812884e53cf8eddfcbc8ced2
Omar Ernesto Cabrera Rosero <omar@od.(none)>
2014-06-12 17:16:08 -0500

    test

---
+++ Src/test/comparacion.sql
@@ -0,0 +1,23 @@
+﻿SELECT bfe.members, bfe.started, bfe.ended, lcm.members, lcm.started, lcm.ended FROM (
+SELECT
+	members, min(started) AS started, max(ended) AS ended 
+FROM	
+	flockbfe
+GROUP BY
+	1
+order by
+	2,1) AS bfe 
+
+LEFT JOIN (
+SELECT
+	members, started, ended 
+FROM	
+	flocklcm
+order by
+	2,1) AS lcm
+
+USING
+	(members,started, ended)
+WHERE
+	lcm.members is NULL
+
---
+++ Src/test/testTapas.sh
@@ -0,0 +1,47 @@
+python3 bfe.py 16 10 5 Tapas.csv test1
+python3 bfe.py 16 10 5 Tapas.csv test2
+python3 bfe.py 18 10 5 Tapas.csv test1
+python3 bfe.py 18 10 5 Tapas.csv test2
+python3 bfe.py 20 10 5 Tapas.csv test1
+python3 bfe.py 20 10 5 Tapas.csv test2
+python3 bfe.py 22 10 5 Tapas.csv test1
+python3 bfe.py 22 10 5 Tapas.csv test2
+python3 bfe.py 24 10 5 Tapas.csv test1
+python3 bfe.py 26 10 5 Tapas.csv test1
+python3 bfe.py 28 10 5 Tapas.csv test1
+python3 lcmFlock.py 16 10 5 Tapas.csv test1
+python3 lcmFlock.py 16 10 5 Tapas.csv test2
+python3 lcmFlock.py 18 10 5 Tapas.csv test1
+python3 lcmFlock.py 18 10 5 Tapas.csv test2
+python3 lcmFlock.py 20 10 5 Tapas.csv test1
+python3 lcmFlock.py 20 10 5 Tapas.csv test2
+python3 lcmFlock.py 22 10 5 Tapas.csv test1
+python3 lcmFlock.py 22 10 5 Tapas.csv test2
+python3 lcmFlock.py 24 10 5 Tapas.csv test1
+python3 lcmFlock.py 26 10 5 Tapas.csv test1
+python3 lcmFlock.py 28 10 5 Tapas.csv test1
+python3 fpFlock.py 16 10 5 Tapas.csv test1
+python3 fpFlock.py 16 10 5 Tapas.csv test2
+python3 fpFlock.py 18 10 5 Tapas.csv test1
+python3 fpFlock.py 18 10 5 Tapas.csv test2
+python3 fpFlock.py 20 10 5 Tapas.csv test1
+python3 fpFlock.py 20 10 5 Tapas.csv test2
+python3 fpFlock.py 22 10 5 Tapas.csv test1
+python3 fpFlock.py 22 10 5 Tapas.csv test2
+python3 fpFlock.py 24 10 5 Tapas.csv test1
+python3 fpFlock.py 26 10 5 Tapas.csv test1
+python3 fpFlock.py 28 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 16 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 16 10 5 Tapas.csv test2
+python3 fpFlockOnline.py 18 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 18 10 5 Tapas.csv test2
+python3 fpFlockOnline.py 20 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 20 10 5 Tapas.csv test2
+python3 fpFlockOnline.py 22 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 22 10 5 Tapas.csv test2
+python3 fpFlockOnline.py 24 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 26 10 5 Tapas.csv test1
+python3 fpFlockOnline.py 28 10 5 Tapas.csv test1
+echo "Finish test Tapas mu=10 delta=5"
+
+
