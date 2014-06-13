d1df771a6f3f9584003fbbbfb2f8a6f5c9971def
poldrosky <omarcabrerarosero@gmail.com>
2014-06-13 02:17:12 -0500

    Update BeijingOriginalTest.sh

--- Src/test/BeijingOriginalTest.sh
+++ Src/test/BeijingOriginalTest.sh
@@ -8,6 +8,8 @@ python3 bfe.py 200 3 3 beijing_original.csv test1
 python3 bfe.py 200 3 3 beijing_original.csv test2
 python3 bfe.py 250 3 3 beijing_original.csv test1
 python3 bfe.py 250 3 3 beijing_original.csv test2
+python3 bfe.py 300 3 3 beijing_original.csv test1
+python3 bfe.py 300 3 3 beijing_original.csv test2
 python3 lcmFlock.py 50 3 3 beijing_original.csv test1
 python3 lcmFlock.py 50 3 3 beijing_original.csv test2
 python3 lcmFlock.py 100 3 3 beijing_original.csv test1
@@ -18,6 +20,8 @@ python3 lcmFlock.py 200 3 3 beijing_original.csv test1
 python3 lcmFlock.py 200 3 3 beijing_original.csv test2
 python3 lcmFlock.py 250 3 3 beijing_original.csv test1
 python3 lcmFlock.py 250 3 3 beijing_original.csv test2
+python3 lcmFlock.py 300 3 3 beijing_original.csv test1
+python3 lcmFlock.py 300 3 3 beijing_original.csv test2
 python3 fpFlock.py 50 3 3 beijing_original.csv test1
 python3 fpFlock.py 50 3 3 beijing_original.csv test2
 python3 fpFlock.py 100 3 3 beijing_original.csv test1
@@ -28,6 +32,8 @@ python3 fpFlock.py 200 3 3 beijing_original.csv test1
 python3 fpFlock.py 200 3 3 beijing_original.csv test2
 python3 fpFlock.py 250 3 3 beijing_original.csv test1
 python3 fpFlock.py 250 3 3 beijing_original.csv test2
+python3 fpFlock.py 300 3 3 beijing_original.csv test1
+python3 fpFlock.py 300 3 3 beijing_original.csv test2
 python3 fpFlockOnline.py 50 3 3 beijing_original.csv test1
 python3 fpFlockOnline.py 50 3 3 beijing_original.csv test2
 python3 fpFlockOnline.py 100 3 3 beijing_original.csv test1
@@ -38,4 +44,6 @@ python3 fpFlockOnline.py 200 3 3 beijing_original.csv test1
 python3 fpFlockOnline.py 200 3 3 beijing_original.csv test2
 python3 fpFlockOnline.py 250 3 3 beijing_original.csv test1
 python3 fpFlockOnline.py 250 3 3 beijing_original.csv test2
+python3 fpFlockOnline.py 300 3 3 beijing_original.csv test1
+python3 fpFlockOnline.py 300 3 3 beijing_original.csv test2
 echo "Finish test Beijin mu=3 delta=3"
